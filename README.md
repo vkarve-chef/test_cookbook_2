386f8e8aef21a31222911e05a17324ab40433935
Vikram Karve <vikram.karve@progress.com>
2021-08-31 15:00:02 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# test_cookbook_2
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook:: test_cookbook_2
+# Recipe:: default
+#
+# Copyright:: 2021, The Authors, All Rights Reserved.
+
+file 'hello2.txt' do
+  content 'Welcome to Chef'
+end
